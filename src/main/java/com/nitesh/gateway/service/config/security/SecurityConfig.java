fd3c252dcc88659c8ab966db37f44144e9155b9d
niteshthakur498 <niteshthakur498@gmail.com>
2025-03-23 22:03:38 +0530

    Added Custom Spring Config with OAuth2.0

---
+++ src/main/java/com/nitesh/gateway/service/config/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.nitesh.gateway.service.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                    exchange.pathMatchers("/actuator/**").permitAll()
+                            .pathMatchers("/test-service/**").permitAll()
+                            .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults())
+                )
+                ;
+        return http.build();
+    }
+}
